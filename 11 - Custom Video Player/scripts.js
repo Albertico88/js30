82714ba2defd87203928ac518196f5c7256fbd33
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-06-02 18:03:01 -0400

    WIP: adding functions

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,1 +1,40 @@
+// Get DOM Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const fill = player.querySelector('.progress__filled');
+const progress = player.querySelector('.progress');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelector('.player__slider');
 
+
+// Build Functions
+// Play
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause button
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+// Skip
+function skip() {
+  console.log('skipping');
+}
+
+
+
+// Hook up Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
