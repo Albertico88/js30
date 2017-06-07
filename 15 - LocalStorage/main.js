2c43e69c7e57b12cc3f8b53af21b3161b0b46d8d
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-06-07 16:18:26 -0400

    populateList function

---
+++ 15 - LocalStorage/main.js
@@ -0,0 +1,33 @@
+const itemsList = document.querySelector('.plates');
+const addItems = document.querySelector('.add-items');
+const items = [];
+
+
+function addItem(event) {
+  event.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text: text,
+    done: false
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  this.reset();
+}
+
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, index) => {
+    return `
+    <li>
+      <input type="checkbox" data-index=${index} id="item${index}" ${plate.done ? 'checked' : ''}/>
+      <label for="item${index}">${plate.text}</label>
+    </li>
+    `;
+  }).join('');
+}
+
+
+
+addItems.addEventListener('submit', addItem);
